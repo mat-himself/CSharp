4e6068fae43c828ec0f1b940fa89d207de14b98f
mat-himself <116955237+mat-himself@users.noreply.github.com>
2022-11-03 08:43:08 +0330

    Add files via upload

---
+++ Taklif1(code).cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Booleans
+{
+    public static void Main()
+    {
+        bool content=true;
+        bool nocontent=false;
+        Console.WriteLine("this is {0}",content);
+        Console.WriteLine("this is {0}",nocontent);
+
+    }
+}
